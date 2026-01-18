9e7871dd188a28f5a4e2a94f24315a88ee7dbae5
Lyes <hachid14@gmail.com>
2026-01-18 21:06:23 +0100

    Small correction

--- app/drivers/urls.py
+++ app/drivers/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'drivers'
 
 urlpatterns = [
-    path('driver/', views.DriverListView.as_view(), name='driver-list'),
-    path('drivers/<int:pk>/', views.DriverDetailView.as_view(), name='driver-details'),
+    path('drivers/', views.DriverListView.as_view(), name='driver-list'),
+    path('driver/<int:pk>/', views.DriverDetailView.as_view(), name='driver-details'),
 ]
